9d7906e22e67de4f9d07cc73380be63bf7d41ecf
Onkarabile Matome <matome.onkarabile8@gmail.com>
2023-10-14 09:05:43 +0200

    addHeadder

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,12 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.getElementById('menu_toggle');
+  const menu = document.getElementById('menu_list');
 
-let load = 0
+  let isOpen = false;
 
-let int = setInterval(blurring, 30)
-
-function blurring() {
-  load++
-
-  if (load > 99) {
-    clearInterval(int)
-  }
-
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
-
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
+  menuToggle.addEventListener('click', function () {
+    isOpen =  !isOpen;
+    menu.classList.toggle('active', isOpen);
+    menuToggle.classList.toggle('active', isOpen);
+  });
+});
